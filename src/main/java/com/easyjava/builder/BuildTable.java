c8acdeea2aad4ab1b5ff819c3c05e66e492e2618
damon <36561263+damon46kai@users.noreply.github.com>
2023-10-29 22:14:11 -0400

    index

--- src/main/java/com/easyjava/builder/BuildTable.java
+++ src/main/java/com/easyjava/builder/BuildTable.java
@@ -1,9 +1,11 @@
 package com.easyjava.builder;
 
 import com.easyjava.bean.Constants;
+import com.easyjava.bean.FieldInfo;
 import com.easyjava.bean.TableInfo;
 import com.easyjava.utils.PropertiesUtils;
 import com.easyjava.utils.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +20,9 @@ public class BuildTable {
     private static Connection conn = null;
 
     private static String SQL_SHOW_TABLE_STATUS = "show table status";
+
+    private static String SQL_SHOW_TABLE_FIELDS = "show full fields from %s";
+
     static {
         String driverName = PropertiesUtils.getString("db.driver.name");
         String url = PropertiesUtils.getString("db.url");
@@ -61,7 +66,11 @@ public class BuildTable {
 
                     tableInfo.setBeanParamName(beanName + Constants.SUFFIX_BEAN_PARAM);
 
-                    logger.info("table:{},comment:{},JavaBean:{},JavaParamBean:{}",tableInfo.getTableName(),tableInfo.getComment(),tableInfo.getBeanName(),tableInfo.getBeanParamName());
+                    List<FieldInfo> fieldInfoList = readFieldInfo(tableInfo);
+
+                    logger.info(fieldInfoList.toString());
+                //    logger.info("table:{},comment:{},JavaBean:{},JavaParamBean:{}",tableInfo.getTableName(),tableInfo.getComment(),tableInfo.getBeanName(),tableInfo.getBeanParamName());
+                readFieldInfo(tableInfo);
                 }
             }catch (Exception e){
                 logger.error("读取表失败");
@@ -92,6 +101,69 @@ public class BuildTable {
         }
 
 
+        private static List<FieldInfo> readFieldInfo(TableInfo tableInfo) {
+            PreparedStatement ps = null;
+            ResultSet fieldResult = null;
+
+            List<FieldInfo> fieldInfoList = new ArrayList();
+            try{
+                ps = conn.prepareStatement(String.format(SQL_SHOW_TABLE_FIELDS, tableInfo.getTableName()));
+                fieldResult = ps.executeQuery();
+                while(fieldResult.next()){
+                    String field = fieldResult.getString("field");
+                    String type = fieldResult.getString("type");
+                    String extra = fieldResult.getString("extra");
+                    String comment = fieldResult.getString("comment");
+                    if(type.indexOf("(")>0){
+                        type = type.substring(0, type.indexOf("("));
+                    }
+
+                    String propertyName  = processField(field, false);
+                    FieldInfo fieldInfo = new FieldInfo();
+                    fieldInfoList.add(fieldInfo);
+
+                    fieldInfo.setFieldName(field);
+                    fieldInfo.setComment(comment);
+                    fieldInfo.setSqlType(type);
+                    fieldInfo.setAutoIncrement("auto_increment".equalsIgnoreCase(extra)?true: false);
+                    fieldInfo.setPropertyName(propertyName);
+                    fieldInfo.setJavaType(processJavaType(type));
+
+                    if(ArrayUtils.contains(Constants.SQL_DATE_TIME_TYPES, type)){
+                        tableInfo.setHaveBigDecimal(true);
+                    }
+                    if(ArrayUtils.contains(Constants.SQL_DATE_TYPES, type)){
+                        tableInfo.setHaveDate(true);
+                    }
+                    if(ArrayUtils.contains(Constants.SQL_DECIMAL_TYPE, type)){
+                        tableInfo.setHaveBigDecimal(true);
+                    }
+
+                    //logger.info("javaType:{}", fieldInfo.getJavaType());
+                   // logger.info("field:{},propertyName:{},type:{},extra:{},comment:{}",field, type, extra, comment);
+
+                 }
+            }catch (Exception e){
+                logger.error("读取表失败");
+            }finally {
+                if(fieldResult!=null){
+                    try {
+                        fieldResult.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(ps!=null){
+                    try {
+                        ps.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+                return fieldInfoList;
+        }
+
         private static String processField(String field, Boolean upperCaseFirstLetter){
             StringBuffer sb = new StringBuffer();
 
@@ -105,4 +177,19 @@ public class BuildTable {
         }
 
 
+        private static String processJavaType(String type){
+            if(ArrayUtils.contains(Constants.SQL_INTEGER_TYPE, type)){
+                return "Integer";
+            }else if(ArrayUtils.contains(Constants.SQL_LONG_TYPE, type)){
+                return "Long";
+            }else if(ArrayUtils.contains(Constants.SQL_STRING_TYPE, type)){
+                return "String";
+            }else if(ArrayUtils.contains(Constants.SQL_DATE_TIME_TYPES,type) || ArrayUtils.contains(Constants.SQL_DATE_TYPES, type)){
+                return "Date";
+            }else if (ArrayUtils.contains(Constants.SQL_DECIMAL_TYPE, type)){
+              return "BigDecimal";
+            } else{
+                throw new RuntimeException("无法识别的类型" + type);
+            }
+        }
 }
